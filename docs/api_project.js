cbf12c081d8c5bad8264a72fddc6890f088b9846
Dimitri <dimitri@formitize.com>
2020-09-01 16:17:31 +1000

    [API Updates] Database functionality

--- docs/api_project.js
+++ docs/api_project.js
@@ -16,7 +16,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-08-31T05:44:28.073Z",
+    "time": "2020-09-01T06:09:54.981Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
