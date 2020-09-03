c46b25f93fa51b94c01e7da8cba8f9bd132c4320
Dimitri <dimitri@formitize.com>
2020-09-03 14:58:23 +1000

    [APIDoc] Wording changes

--- docs/api_project.js
+++ docs/api_project.js
@@ -16,7 +16,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-09-03T04:29:32.287Z",
+    "time": "2020-09-03T04:57:20.249Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
