febc5e134512d4a34fd5626e7ec09670b3da535c
Dimitri <dimitri@formitize.com>
2020-10-29 16:43:41 +1100

    [Docs] tidyup

--- docs/api_project.js
+++ docs/api_project.js
@@ -16,7 +16,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-10-29T05:33:05.156Z",
+    "time": "2020-10-29T05:42:52.742Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
