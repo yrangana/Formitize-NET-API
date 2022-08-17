39c85ca35a5187b04d6452a799aa2686f133145f
Dimitri <dimitri@formitize.com>
2022-08-17 17:02:48 +1000

    feat(api-invoice): updated docs to include invoice list and get

--- docs/api_project.js
+++ docs/api_project.js
@@ -16,7 +16,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-12-07T04:09:14.554Z",
+    "time": "2022-08-17T06:55:39.577Z",
     "url": "https://apidocjs.com",
     "version": "0.29.0"
   }
